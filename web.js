3278d49889f1ab586f4612a91534d23f750df8c2
Seth Pollack <seth@pollackphoto.com>
2012-01-11 09:33:46 -0800

    back to node v0.4.7; path handling

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/region/:id', function(request, response) {
+  response.send('Hello ' + request.params.id + '!\n');
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
