d063bbe666d2cbce0aed203ec30b517acb10668e
Seth Pollack <seth@pollackphoto.com>
2012-01-11 09:37:23 -0800

    add comments

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ app.get('/region/:id', function(request, response) {
   response.send('Hello ' + request.params.id + '!\n');
 });
 
+// use the value from the PORT env variable if available
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
