3a0e8b8508f276c201939053f4c01c949f96a1a6
Seth Pollack <seth@pollackphoto.com>
2012-01-11 12:59:46 -0800

    requesting an external URL

--- web.js
+++ web.js
@@ -1,13 +1,36 @@
+//
+// launch the web server
+//
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/region/:id', function(request, response) {
-  response.send('Hello ' + request.params.id + '!\n');
-});
+app.get('/region/:id', onRequest);
 
 // use the value from the PORT env variable if available
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
+
+//
+// request handling
+//
+
+var http = require('http');
+
+function onRequest(request, response) {
+	var options = {
+	  host: 'www.google.com',
+	  port: 80,
+	  path: '/index.html'
+	};
+	
+	http.get(options, function(clientResponse) {
+		console.log("Got response: " + clientResponse.statusCode);
+		response.send('Hello ' + request.params.id + '!\n');
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
+}
