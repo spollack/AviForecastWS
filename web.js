fb628ed695de4c03a384c6b1aa6095eeee90128b
Seth Pollack <seth@pollackphoto.com>
2012-01-13 07:19:35 -0800

    cleanup of naming

--- web.js
+++ web.js
@@ -7,13 +7,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 // path mapping
-app.get('/region/:id', onRequest);
+app.get('/region/:regionId', onRequest);
 
 // use the value from the PORT env variable if available
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	console.log('listening on ' + port);
-});
+app.listen(port,
+    function() {
+	    console.log('listening on ' + port);
+    }
+);
 
 //
 // request handling
@@ -26,23 +28,25 @@ var request = require('request');
 // origRequest/origResponse are the client-originated HTTP request; not to be confused with
 // the server to server request that we initiate here to query the appropriate forecast site
 function onRequest(origRequest, origResponse) {
-    var id = origRequest.params.id;
-	var URL = getURLFromId(id);
+    var regionId = origRequest.params.regionId;
+	var URL = getURLForRegionId(regionId);
 
     if (!URL) {
-        console.log('invalid id received from client; id: ' + id);
+        console.log('invalid regionId received from client; regionId: ' + regionId);
         sendNoDataAvailableResponse(origResponse);
     } else {
-        request(URL, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                console.log('successful response; id: ' + id + '; URL: ' + URL);
-                var aviLevel = parseForecast(body, id);
-                sendDataResponse(origResponse, aviLevel);
-            } else {
-                console.log('error response; id: ' + id + '; URL: ' + URL + '; status code: ' + response.statusCode + '; error: ' + error);
-                sendNoDataAvailableResponse(origResponse);
+        request(URL,
+            function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log('successful response; regionId: ' + regionId + '; URL: ' + URL);
+                    var aviLevel = parseForecast(body, regionId);
+                    sendDataResponse(origResponse, aviLevel);
+                } else {
+                    console.log('error response; regionId: ' + regionId + '; URL: ' + URL + '; status code: ' + response.statusCode + '; error: ' + error);
+                    sendNoDataAvailableResponse(origResponse);
+                }
             }
-        });
+        );
     }
 }
 
@@ -58,12 +62,12 @@ function sendDataResponse(origResponse, aviLevel) {
     origResponse.send(responseBody);
 }
 
-function getURLFromId(id) {
+function getURLForRegionId(regionId) {
     // NOTE this will have to be extended to support other avalanche forecast centers
-    return 'http://www.nwac.us/forecast/avalanche/current/zone/' + id + '/';
+    return 'http://www.nwac.us/forecast/avalanche/current/zone/' + regionId + '/';
 }
 
-function parseForecast(body, id) {
+function parseForecast(body, regionId) {
 	var aviLevel = 0; 
 	
 	// scrape the website for the data we need
@@ -74,7 +78,7 @@ function parseForecast(body, id) {
     // the capture group from the regex will be in slot 1 in the array
 	if (match && match.length > 1) {
         var matchLevel = match[1].toLowerCase();
-		console.log('found regex match; id: ' + id + '; match: ' + matchLevel);
+		console.log('found regex match; regionId: ' + regionId + '; match: ' + matchLevel);
 		switch(matchLevel) {
 			case 'low':
 				aviLevel = 1;
@@ -95,7 +99,7 @@ function parseForecast(body, id) {
 				break;
 		}
 	} else {
-		console.log('no regex match; id: ' + id);
+		console.log('no regex match; regionId: ' + regionId);
 	}
 	
 	return aviLevel; 
