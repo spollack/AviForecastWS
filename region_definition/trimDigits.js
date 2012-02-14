8274e9eb3757f38adcf6023d5877c9233fd5d88d
Seth Pollack <seth@pollackphoto.com>
2012-02-13 16:05:39 -0800

    trim digits in regions.json for file size reduction (57% reduction)

---
+++ region_definition/trimDigits.js
@@ -0,0 +1,26 @@
+//
+// required packages
+//
+
+var fs = require('fs');
+
+trimDigitsInRegionsJSONFile('../public/v1/regions.json','./new_regions.json',4);
+
+function trimDigitsInRegionsJSONFile(inputFilePath, outputFilePath, digits) {
+    var input = fs.readFileSync(inputFilePath, 'utf8');
+    var output = trimDigits(input, digits);
+    fs.writeFileSync(outputFilePath, output, 'utf8');
+}
+
+function trimDigits(input, digits) {
+    var regions = JSON.parse(input);
+
+    for (var i = 0; i < regions.length; i++) {
+        for (var j = 0; j < regions[i].points.length; j++) {
+            regions[i].points[j].lat = parseFloat(regions[i].points[j].lat).toFixed(digits);
+            regions[i].points[j].lon = parseFloat(regions[i].points[j].lon).toFixed(digits);
+        }
+    }
+
+    return (JSON.stringify(regions, null, 4));
+}
