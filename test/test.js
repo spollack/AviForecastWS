29f7d9d5d256199d28c3133b3de617442893517d
Seth Pollack <seth@pollackphoto.com>
2012-11-20 18:29:24 -0800

    added another nwac test

--- test/test.js
+++ test/test.js
@@ -205,6 +205,21 @@ describe('parseForecast_nwac', function(){
             forecast[2].aviLevel.should.equal(2);
         })
     })
+    describe('file003.json', function(){
+        it('should return the correct forecast details', function(){
+            var forecast = forecasts.parseForecast_nwac(fs.readFileSync('test/data/nwac/file003.json','utf8'),
+                forecasts.getRegionDetailsForRegionId('nwac_5'));
+
+            should.exist(forecast);
+            forecast.length.should.equal(3);
+            forecast[0].date.should.equal('2012-11-20');
+            forecast[1].date.should.equal('2012-11-21');
+            forecast[2].date.should.equal('2012-11-22');
+            forecast[0].aviLevel.should.equal(3);
+            forecast[1].aviLevel.should.equal(3);
+            forecast[2].aviLevel.should.equal(2);
+        })
+    })
 })
 
 describe('parseForecast_cac', function(){
