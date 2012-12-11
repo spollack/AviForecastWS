997983ace12a25388db1bc2939574495d73f0204
Seth Pollack <seth@pollackphoto.com>
2012-12-11 13:44:32 -0800

    snfac.json

--- region_definition/KMLtoJSON.js
+++ region_definition/KMLtoJSON.js
@@ -5,8 +5,9 @@
 var fs = require('fs');
 
 
+KMLFileToJSONFile('snfac/Advisory Region Map 2011 - zones.kml','snfac/snfac.json','snfac','http://www.sawtoothavalanche.com/adv_current.php');
 //KMLFileToJSONFile('sac/SACForecastArea.kml','sac/sac.json','sac','http://www.sierraavalanchecenter.org/advisory');
-KMLFileToJSONFile('uac/uac.kml','uac/uac.json','uac','http://utahavalanchecenter.org/advisory/');
+//KMLFileToJSONFile('uac/uac.kml','uac/uac.json','uac','http://utahavalanchecenter.org/advisory/');
 // KMLFileToJSONFile('nwac_regions.kml','test_nwac_regions.json','nwac','http://www.nwac.us/forecast/avalanche/current/zone/');
 // KMLFileToJSONFile('canada_regions.kml','canada_regions.json','canada','');
 
