3beb134a003b661ca3588bdd389d7f73eb28c9ca
Seth Pollack <seth@pollackphoto.com>
2013-12-05 17:54:38 -0800

    trim digits on the nwac source file

--- region_definition/trimDigits.js
+++ region_definition/trimDigits.js
@@ -4,6 +4,7 @@
 var fs = require('fs');
 
 // trimDigitsInRegionsJSONFile('../public/v1/regions.json', '../public/v1/regions.json', 3);
+trimDigitsInRegionsJSONFile('nwac/nwac_regions.json', 'nwac/nwac_regions.json', 3);
 // trimDigitsInRegionsJSONFile('Canada_all/canada_regions_wb_fix_simplified.json', 'Canada_all/canada_regions_wb_fix_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('btac/btac_simplified.json', 'btac/btac_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('sac/sac_simplified.json', 'sac/sac_simplified.json', 3);
@@ -16,8 +17,8 @@ var fs = require('fs');
 // trimDigitsInRegionsJSONFile('ipac/ipac_simplified.json', 'ipac/ipac_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('cnfaic/cnfaic_simplified.json', 'cnfaic/cnfaic_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('aac/aac_simplified.json', 'aac/aac_simplified.json', 3);
-//trimDigitsInRegionsJSONFile('jac/jac_simplified.json', 'jac/jac_simplified.json', 3);
-trimDigitsInRegionsJSONFile('haic/haic_simplified.json', 'haic/haic_simplified.json', 3);
+// trimDigitsInRegionsJSONFile('jac/jac_simplified.json', 'jac/jac_simplified.json', 3);
+// trimDigitsInRegionsJSONFile('haic/haic_simplified.json', 'haic/haic_simplified.json', 3);
 
 
 function trimDigitsInRegionsJSONFile(inputFilePath, outputFilePath, digits) {
