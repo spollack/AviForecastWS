0b30e4ee0f7d7ea26df4f301ec381f61b047ebc5
Seth Pollack <seth@pollackphoto.com>
2013-12-26 17:57:27 -0800

    VAC region

--- region_definition/KMLtoJSON.js
+++ region_definition/KMLtoJSON.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 
 // KMLFileToJSONFile('nwac/nwac_regions.kml','nwac/nwac_regions.json','nwac','http://www.nwac.us/forecast/avalanche/current/zone/');
-KMLFileToJSONFile('Canada_all/canada_regions_simplified.kml','Canada_all/canada_regions_simplified.json','cac','http://www.avalanche.ca/cac/bulletins/latest/');
+// KMLFileToJSONFile('Canada_all/canada_regions_simplified.kml','Canada_all/canada_regions_simplified.json','cac','http://www.avalanche.ca/cac/bulletins/latest/');
 // KMLFileToJSONFile('caic/caic_simplified.kml','caic/caic_simplified.json','caic','http://avalanche.state.co.us/pub_bc_avo.php?zone_id=');
 // KMLFileToJSONFile('uac/uac_simplified.kml','uac/uac_simplified.json','uac','http://utahavalanchecenter.org/advisory/');
 // KMLFileToJSONFile('sac/SACForecastArea_simplified.kml','sac/sac_simplified.json','sac','http://www.sierraavalanchecenter.org/advisory');
@@ -20,6 +20,7 @@ KMLFileToJSONFile('Canada_all/canada_regions_simplified.kml','Canada_all/canada_
 // KMLFileToJSONFile('jac/Forecast Area.kml','jac/jac_simplified.json','jac','http://juneau.org/avalanche/');
 // KMLFileToJSONFile('haic/haic_simplified.kml','haic/haic_simplified.json','haic','http://alaskasnow.org/haines/conditions.html');
 // KMLFileToJSONFile('fac/fac_simplified.kml','fac/fac_simplified.json','fac','http://www.flatheadavalanche.org/category/advisories/');
+KMLFileToJSONFile('vac/vac_simplified.kml','vac/vac_simplified.json','vac','http://www.valdezavalanchecenter.org/category/bulletin/');
 
 
 function KMLFileToJSONFile(KMLFileName, JSONFileName, regionPrefix, URLPrefix) {
