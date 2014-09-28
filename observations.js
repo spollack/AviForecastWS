f8c79be4b1d915a459c12336932403c80da21f35
Seth Pollack <seth@pollackphoto.com>
2014-09-28 15:15:29 -0700

    start at observations handling

---
+++ observations.js
@@ -0,0 +1,18 @@
+//
+// exports
+//
+var observations = module.exports = {};
+
+
+//
+// required packages
+//
+var winston = require('winston');
+var moment = require('moment');
+var _ = require('underscore');
+var async = require('async');
+
+
+observations.processObservation = function(observation, onCompletion) {
+
+};
