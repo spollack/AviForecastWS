9d40b93e5e2c55032b20ade1d76c4b342a333b35
Seth Pollack <seth@pollackphoto.com>
2014-09-28 15:18:59 -0700

    cleanup

--- observations.js
+++ observations.js
@@ -8,9 +8,6 @@ var observations = module.exports = {};
 // required packages
 //
 var winston = require('winston');
-var moment = require('moment');
-var _ = require('underscore');
-var async = require('async');
 
 
 observations.processObservation = function(observation, onCompletion) {
