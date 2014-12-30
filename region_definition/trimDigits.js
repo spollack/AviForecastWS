8037355762bc04f1e5927043af2a0a20b510ffe2
Seth Pollack <seth@pollackphoto.com>
2014-12-30 08:10:28 -0800

    update Banff/Yoho/Kootenay region

--- region_definition/trimDigits.js
+++ region_definition/trimDigits.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 // trimDigitsInRegionsJSONFile('../public/v1/regions.json', '../public/v1/regions.json', 3);
 // trimDigitsInRegionsJSONFile('nwac/nwac_regions.json', 'nwac/nwac_regions.json', 3);
-// trimDigitsInRegionsJSONFile('Canada_all/canada_regions_simplified.json', 'Canada_all/canada_regions_simplified.json', 3);
+trimDigitsInRegionsJSONFile('Canada_all/canada_regions_simplified.json', 'Canada_all/canada_regions_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('btac/btac_simplified.json', 'btac/btac_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('sac/sac_simplified.json', 'sac/sac_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('uac/uac_simplified.json', 'uac/uac_simplified.json', 3);
@@ -18,7 +18,7 @@ var fs = require('fs');
 // trimDigitsInRegionsJSONFile('cnfaic/cnfaic_simplified.json', 'cnfaic/cnfaic_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('aac/aac_simplified.json', 'aac/aac_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('jac/jac_simplified.json', 'jac/jac_simplified.json', 3);
-trimDigitsInRegionsJSONFile('haic/haic_simplified.json', 'haic/haic_simplified.json', 3);
+// trimDigitsInRegionsJSONFile('haic/haic_simplified.json', 'haic/haic_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('fac/fac_simplified.json', 'fac/fac_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('vac/vac_simplified.json', 'vac/vac_simplified.json', 3);
 // trimDigitsInRegionsJSONFile('mwac/mwac_simplified.json', 'mwac/mwac_simplified.json', 3);
